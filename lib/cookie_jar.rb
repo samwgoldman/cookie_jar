87038b9d7151f4d9beccdf19085012545ff85ccb
Sam Goldman <samwgoldman@gmail.com>
2013-01-26 13:42:09 -1000

    Cookie jar evicts cookies set to expire in the past

--- lib/cookie_jar.rb
+++ lib/cookie_jar.rb
@@ -1,13 +1,21 @@
+require "cookie"
+
 class CookieJar
   def initialize
     @cookies = []
   end
 
-  def set_cookie(cookie)
-    @cookies << cookie
+  def set_cookie(cookie_string)
+    cookie = Cookie.parse(cookie_string)
+    existing_cookie = @cookies.find { |c| c.name == cookie.name }
+    if existing_cookie && cookie.expired?
+      @cookies.delete(existing_cookie)
+    else
+      @cookies << cookie
+    end
   end
 
   def cookie
-    @cookies.map { |cookie| cookie.split(";").first }.join("; ")
+    @cookies.map { |cookie| "#{cookie.name}=#{cookie.value}" }.join("; ")
   end
 end
--- spec/cookie_jar_spec.rb
+++ spec/cookie_jar_spec.rb
@@ -18,4 +18,16 @@ describe CookieJar do
     jar.set_cookie("lang=en-US; Path=/; Domain=example.com")
     jar.cookie.should eq("SID=31d4d96e407aad42; lang=en-US")
   end
+
+  context "with a couple cookies" do
+    before do
+      jar.set_cookie("SID=31d4d96e407aad42; Path=/; Secure; HttpOnly")
+      jar.set_cookie("lang=en-US; Path=/; Domain=example.com")
+    end
+
+    it "removes cookies if set to expire in the past" do
+      jar.set_cookie("lang=en-US; Expires=Sun, 06 Nov 1994 08:49:37 GMT")
+      jar.cookie.should eq("SID=31d4d96e407aad42")
+    end
+  end
 end
